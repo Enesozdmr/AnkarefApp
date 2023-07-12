1cac28d4affd037784af701ce7dd031e33a69758
Muhammed ÖZDEMİR <enesozdmr@outlook.com>
2023-07-12 14:17:52 +0300

    Initial commit

---
+++ AnkarefApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AnkarefApp.Models;
+
+namespace AnkarefApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    // GET
+    public IActionResult Index()
+    {
+        if (HttpContext.Session.GetString("UserId") != null)
+        {
+            return RedirectToAction("Job", "Home");
+        }
+        
+        return View();
+    }
+
+    public string Job()
+    {
+        return "başarıyla giriş yapıldı";
+    }
+
+    // POST
+    [HttpPost]
+    public IActionResult Login(string inputEmail, string inputPassword)
+    {
+        if (inputEmail == "admin@example.com" && inputPassword == "password")
+        {
+            HttpContext.Session.SetString("UserId", inputEmail);
+            return RedirectToAction("Job", "Home");
+        }
+
+        ViewBag.ErrorMessage = "Invalid username or password.";
+        
+        return View("Index");
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ AnkarefApp/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(1800);
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCookiePolicy();
+app.UseSession();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
